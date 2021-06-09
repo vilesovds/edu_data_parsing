645be2470d3b6938f1ce2982033104b5d29d764e
Dmitry Vilesov <d.vilesov@.r2cmarket.com>
2021-06-09 13:41:15 +0300

    fixed waiting input password button added parsing letter to subject, sender, and body text

--- HW7/part1.py
+++ HW7/part1.py
@@ -22,7 +22,9 @@ def parse_mail(driver, login, password):
     results = []
 
     def _login():
-        login_btn = driver.find_element_by_xpath(login_xpath)
+        login_btn = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, login_xpath))
+        )
         logger.debug(login_btn.text)
         login_btn.click()
         # wait loading iframe
@@ -39,16 +41,40 @@ def parse_mail(driver, login, password):
         logger.debug(f'attempt to use login {login}')
         user_name_input.send_keys(login)
         form.find_element_by_xpath('.//button[@data-test-id="next-button"]').click()
-        form.find_element_by_xpath('.//input[@name="password"]').send_keys(password)
+        pass_input = form.find_element_by_xpath('.//input[@name="password"]')
+        WebDriverWait(driver, 10).until(
+            EC.visibility_of(pass_input)
+        )
+        pass_input.send_keys(password)
         form.find_element_by_xpath('.//button[@data-test-id="submit-button"]').click()
         # switch back
         driver.switch_to.default_content()
 
     def _parse_link(_url):
+        ret = {}
         logger.debug(f'parsing {_url}')
         driver.open_in_new_tab(_url)
-        input('press enter to continue: ')
+        # wait content
+        layout_thread = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, ('//div[@class="layout__content"]')))
+        )
+
+        subject = layout_thread.find_element_by_xpath('.//h2[contains(@class, "thread__subject")]').text
+        logger.debug(f' letter subject: {subject}')
+        ret['subject'] = subject
+        sender = layout_thread.find_element_by_xpath('.//span[@class="letter-contact"]')
+        sender_name = sender.text
+        sender_email = sender.get_attribute('title')
+        logger.debug(f'sender: {sender_name} {sender_email}')
+        ret['sender_name'] = sender_name
+        ret['sender_email'] = sender_email
+        date_str = layout_thread.find_element_by_xpath('.//div[@class="letter__date"]').text
+        logger.debug(date_str)
+        letter_body = layout_thread.find_element_by_xpath('.//div[@class="letter-body"]').text
+        logger.debug(letter_body)
+        ret['body_text'] = letter_body
         driver.close_but_index(0)
+        return ret
 
     try:
         driver.get(url)
